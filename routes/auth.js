f928f92db05d4c79adf41d5fbc5e2ca93718cff2
Ariel <ariel_pons@hotmail.com>
2022-02-11 09:43:07 -0300

    Se agrega renewToken

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
-const { login, googleSignIn } = require("../controllers/auth");
+const { login, googleSignIn, renewToken } = require("../controllers/auth");
+const { validarJWT } = require("../middlewares/validar-jwt");
 const { validarCampos } = require("../middlewares/validar.campos");
 
 const router = Router();
@@ -20,5 +21,9 @@ router.post('/google',
     validarCampos
     ],
     googleSignIn
+)
+router.get('/renew',
+    validarJWT,
+    renewToken
 )
 module.exports = router;
