18bb6fb87d30bb826679187e80e64523e3ed2518
Ariel <ariel_pons@hotmail.com>
2022-02-14 15:47:00 -0300

    renewToken devuelve el usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -80,9 +80,11 @@ const renewToken = async(req, res = response)=>{
     const uid = req.uid;
      //Generar el TOKEN - JWT
     const token = await generarJWT( uid );
+    const usuario = await Usuario.findById(uid);
     res.json({
         ok:true,
-        token
+        token,
+        usuario
     })
 }
 module.exports = {
