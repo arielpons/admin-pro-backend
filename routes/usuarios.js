2640ff1e31a60ea70aa28734c64b11e616d534bd
Ariel <ariel_pons@hotmail.com>
2022-02-23 14:21:00 -0300

    Validación Rol

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,25 +2,37 @@ const { Router } = require("express");
 const { getUsuarios, postUsuarios, putUsuarios, deleteUsuario } = require("../controllers/usuarios");
 const { check } = require('express-validator');
 const { validarCampos } = require("../middlewares/validar.campos");
-const { validarJWT } = require("../middlewares/validar-jwt");
+const { validarJWT, validarRole, validarRoleOMismoUsuario } = require("../middlewares/validar-jwt");
 
 const router = Router();
 
 router.get("/", validarJWT, getUsuarios);
-router.post("/",[
-    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
-    check('password', 'El password es obligatorio').not().isEmpty(),
-    check('email', 'El email es obligatorio').isEmail().not().isEmpty(),
-    validarCampos
-], postUsuarios);
-router.put("/:id",[
-    validarJWT,
-    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
-    check('role', 'El role es obligatorio').not().isEmpty(),
-    check('email', 'El email es obligatorio').isEmail().not().isEmpty(),
-    validarCampos
-], putUsuarios);
+
+router.post("/",
+    [
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('password', 'El password es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail().not().isEmpty(),
+        validarCampos
+    ],
+    postUsuarios);
+
+router.put("/:id",
+    [
+        validarJWT,
+        validarRoleOMismoUsuario,
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('role', 'El role es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail().not().isEmpty(),
+        validarCampos
+    ],
+    putUsuarios);
+
 router.delete("/:id",
-    validarJWT,
+    [
+        validarJWT,
+        validarRole
+    ],
     deleteUsuario)
+    
 module.exports = router;
