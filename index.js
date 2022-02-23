f5de1a5c8d989defd2ddd5ae418123fcd4966849
Ariel <ariel_pons@hotmail.com>
2022-02-23 16:51:02 -0300

    Path fixed

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use('/api/login', require('./routes/auth'));
 
 // Lo último
 app.get('*', (req, res) => {
-    res.sendFile( path.resolve( __dirname, '.public/index.html' ) );
+    res.sendFile( path.resolve( __dirname, 'public/index.html' ) );
 });
 
 app.listen(process.env.PORT || 3000, ()=>{
