4a912110229698872c9bde8618d2760986035f5b
Ariel <ariel_pons@hotmail.com>
2022-02-24 15:01:11 -0300

    Se soluciona Google login

--- helpers/google.verify.js
+++ helpers/google.verify.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const {OAuth2Client} = require('google-auth-library');
 const client = new OAuth2Client(process.env.GOOGLE_ID);
 const googleVerify = async(token)=> {
     const ticket = await client.verifyIdToken({
-        idToken: token,
+      idToken: token,
       audience: process.env.GOOGLE_ID,  // Specify the CLIENT_ID of the app that accesses the backend
       // Or, if multiple clients access the backend:
       //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
